66a834419bc24137e1f1996bfb3b1a7bc0e9833e
Glitch (hello-express) <none>
2020-12-15 07:10:37 +0000

    ☺️🚨 Checkpoint ./bot.js:21235391/4528 ./komutlar/kayıt-erkek.js:21235391/2422 ./komutlar/isimler.js:21235391/2029 ./komutlar/kayıt-kadın.js:21235391/6502

--- komutlar/kayıt-erkek.js
+++ komutlar/kayıt-erkek.js
@@ -21,15 +21,29 @@ if(!savelogs) return message.channel.send('Save log ayarlanmamış.')
 
 
 
-let member = message.guild.member(message.mentions.user.first() || message.guild.members.cache.get(args[0]))
-if(!member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send('Kendinden üst/aynı pozisyonda birisini kayıt edemezsin.')
+const member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
+
 if(!member) return message.channel.send(`Bir kullanıcı belirt.`)
 if(member.id === message.author.id) return message.channel.send('Kendini kayıt edemezsin.')
 if(member.id === client.user.id) return message.channel.send('Botu kayıt edemezsin.')
 if(member.id === message.guild.OwnerID) return message.channel.send('Sunucu sahibini kayıt edemezsin.')
-
-
-let tag = 'tag' 
+if(member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send(`Bu kullanıcı sizden üst/aynı pozsiyondadır.`)
+  
+if(!args[0]) return message.channel.send('Bir kullanıcı belirt')  
+let timereplace = args[0];
+let time = timereplace.replace(/y/, ' yıl').replace(/d/, ' gün').replace(/s/, ' saniye').replace(/m/, ' dakika').replace(/h/, ' saat') 
+ datab.add('case', 1)
+ const sadxstg = await datab.fetch('case')
+ var tarih = new Date(Date.now())
+ var tarih2 = ms(timereplace)
+ var tarih3 = Date.now() + tarih2 + 1296000000
+ let ay = moment(Date.now()+1296000000).format("MM")
+ let gün = moment(Date.now()+1296000000).format("DD")
+ let saat = moment(Date.now()+1296000000).format("HH:mm:ss")
+ let yıl = moment(Date.now()+1296000000).format("YYYY")
+ let kayıtsaat = `\`${gün} ${ay.replace(/01/, 'Ocak').replace(/02/, 'Şubat').replace(/03/, 'Mart').replace(/04/, 'Nisan').replace(/05/, 'Mayıs').replace(/06/, 'Haziran').replace(/07/, 'Temmuz').replace(/08/, 'Ağustos').replace(/09/, 'Eylül').replace(/10/, 'Ekim').replace(/11/, 'Kasım').replace(/12/, 'Aralık')} ${saat} (${yıl})\``
+ 
+let tag = 'STRG' 
 let name = args[1]
 let age = Number(args[2])
 if(!name) return message.channel.send('Bir isim belirt.')
@@ -59,13 +73,13 @@ message.channel.send(embed)
 
 const saveall = new Discord.MessageEmbed()
 .setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
-.addField(`Kayıt Eden`, `${message.author}`)
-.addField(`Kullanıcı`, `${member}`)
-.addField(`Roller`, `${erkek}, ${erkek2}`)
-.addField(`İsim`, `\`${tag} ${name} | ${age}\``)
-.addField(`Kanal`, `\`${message.channel.name}\``)
-.addField(`Kayıtları`, `\`${alldata}\``)
-.addField(`Kayıt Saat`, `\`\``)
+.addField(`Kayıt Eden`, `${message.author}`, true)
+.addField(`Kullanıcı`, `${member}`, true)
+.addField(`Roller`, `${erkek}, ${erkek2}`, true)
+.addField(`İsim`, `\`${tag} ${name} | ${age}\``, true)
+.addField(`Kanal`, `\`${message.channel.name}\``, true)
+.addField(`Kayıtları`, `\`${alldata}\``, true)
+.addField(`Kayıt Saat`, `\`${kayıtsaat}\``, true)
 .setFooter('Striga Code')
 savelogs.send(saveall)
 
