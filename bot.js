85d9e13dcef54abbd60b007fe4e07edbbdb0f1df
Glitch (hello-express) <none>
2021-01-16 13:10:41 +0000

    🐈🛣 Checkpoint ./bot.js:26488123/37778

--- bot.js
+++ bot.js
@@ -115,62 +115,10 @@ client.on('error', e => {
 
 client.login(ayarlar.token);
 
-//-----------------------GİRENE-ROL-VERME----------------------\\     STG
-
-client.on("guildMemberAdd", member => {
-  member.roles.add('UNREGISTER ID'); // UNREGİSTER ROLÜNÜN İDSİNİ GİRİN
-});
-
-//-----------------------GİRENE-ROL-VERME----------------------\\     STG
-
-
-//------------------------HOŞGELDİN-EMBEDSİZ-----------------------\\     STG
-
-client.on("guildMemberAdd", member => {
-    require("moment-duration-format")
-      var üyesayısı = member.guild.members.cache.size.toString().replace(/ /g, "    ")
-      var üs = üyesayısı.match(/([0-9])/g)
-      üyesayısı = üyesayısı.replace(/([a-zA-Z])/g, "bilinmiyor").toLowerCase()
-      if(üs) {
-        üyesayısı = üyesayısı.replace(/([0-9])/g, d => {
-          return {
-            '0': `<a:sifir:799983487557435434>`,
-            '1': `<a:bir:799983456393232404>`,
-            '2': `<a:iki:799983486332174387>`,
-            '3': `<a:uc:799983487377342474>`,
-            '4': `<a:dort:799983485909467166>`,
-            '5': `<a:bes:799983456649478174>`,
-            '6': `<a:alti:799983444746698752>`,
-            '7': `<a:yedi:799983484504113184>`,
-            '8': `<a:sekiz:799983487914082314>`,
-            '9': `<a:dokuz:799983474677121034>`}[d];})}
-    const kanal = member.guild.channels.cache.find(r => r.id === "799980199658651689");
-    let user = client.users.cache.get(member.id);
-    require("moment-duration-format");
-      const kurulus = new Date().getTime() - user.createdAt.getTime();  
-     const gecen = moment.duration(kurulus).format(` YY **[Yıl,]** DD **[Gün,]** HH **[Saat,]** mm **[Dakika,]** ss **[Saniye]**`) 
-    var kontrol;
-  if (kurulus < 1296000000) kontrol = 'Hesap Durumu: Güvenilir Değil.'
-  if (kurulus > 1296000000) kontrol = 'Hesap Durumu: Güvenilir Gözüküyor.'
-    moment.locale("tr");
-    kanal.send(`
-\`˃\` Sunucumuza hoş geldin <@`+ member + `>, hesabın \``+gecen+`\` tarihinde oluşturulmuş ve `+kontrol+`
-
-     \`˃\` Kayıt olmak için \`Confirmation\` odalarına geçip <@&788898307320840242> yetkilisine teyit vererek kayıt olabilirsin.
-
-          \`˃\` Sunucumuz \`Tagınız\` tagını kullanıcı adına ekleyerek bizelerden birisi olabilirsin.
-
-     \`˃\` Seninle beraber sunucumuz toplam `+üyesayısı+ ` kişiye ulaştı bizi şereflendirdin ! 
-
-\`˃\` İçerize keyifli vakitler geçirmeni diler, sunucumuzun <#799980199658651689> kısmına göz atmanı tavsiye ederiz.`)});
-  
-
-//------------------------HOŞGELDİN-EMBEDSİZ-----------------------\\     STG
-
 
 //------------------------HOŞGELDİN-EMBEDLİ-----------------------\\     STG
 
-client.on("guildMemberAdd", (member, message) => {
+client.on("guildMemberAdd", member => {
     require("moment-duration-format")
       var üyesayısı = member.guild.members.cache.size.toString().replace(/ /g, "    ")
       var üs = üyesayısı.match(/([0-9])/g)
@@ -189,7 +137,7 @@ client.on("guildMemberAdd", (member, message) => {
             '8': `<a:sekiz:799983487914082314>`,
             '9': `<a:dokuz:799983474677121034>`}[d];})}
       const kanal = member.guild.channels.cache.find(r => r.id === "799980199658651689");
-      let register = 'REGİSTER ROL ID AYIKS'
+      let register = '788898307320840242'
     let user = client.users.cache.get(member.id);
     require("moment-duration-format");
       const kurulus = new Date().getTime() - user.createdAt.getTime();  
@@ -199,7 +147,7 @@ client.on("guildMemberAdd", (member, message) => {
   if (kurulus > 1296000000) kontrol = 'Hesap Durumu: Güvenilir Gözüküyor.'
     moment.locale("tr");
   const embed = new Discord.MessageEmbed()
-  .setAuthor(message.guild.name, message.guild.iconURL({dynamic:true}))
+  .setAuthor(member.guild.name, member.guild.iconURL({dynamic:true}))
   .setDescription(`
   <@`+member.id+`> Sunucumuza Katıldı ! 
   
@@ -209,89 +157,10 @@ client.on("guildMemberAdd", (member, message) => {
   
   Sunucumuzun kurallarına uymayı unutma, kurallarımızı okumanı tavsiye ederiz.
 
-  İçeride keyifli vakitler geçirmeni dileriz.
-  `)
+  İçeride keyifli vakitler geçirmeni dileriz.`)
+  .setImage(`https://data.whicdn.com/images/138550574/original.gif`)
   kanal.send(embed)
-
+  kanal.send(`<@&${register}>`)
 });
 //------------------------HOŞGELDİN-EMBEDLİ-----------------------\\     STG
 
-
-//------------------------ŞÜPHELİ-HESAP-----------------------\\     STG
-
-client.on("guildMemberAdd", member => {
-    var moment = require("moment")
-    require("moment-duration-format")
-    moment.locale("tr")
-     var {Permissions} = require('discord.js');
-     var x = moment(member.user.createdAt).add(7, 'days').fromNow()
-     var user = member.user
-     x = x.replace("birkaç saniye önce", " ")
-     if(!x.includes("önce") || x.includes("sonra") ||x == " ") {
-    const kytsz = member.guild.roles.cache.find(r => r.id === "KAYITSIZ ROL ID") 
-     var rol = member.guild.roles.cache.get("ŞÜPHELİ ROL ID") // ŞÜPHELİ HESAP ROLÜNÜN İDSİNİ GİRİN
-     var kayıtsız = member.guild.roles.cache.get(kytsz) // UNREGİSTER ROLÜNÜN İDSİNİ GİRİN
-     member.roles.add(rol)
-     member.roles.remove(kytsz)
-
-  member.user.send('Selam Dostum Ne Yazık ki Sana Kötü Bir Haberim Var Hesabın 1 Hafta Gibi Kısa Bir Sürede Açıldığı İçin Fake Hesap Katagorisine Giriyorsun Lütfen Bir Yetkiliyle İletişime Geç Onlar Sana Yardımcı Olucaktır.')
-  setTimeout(() => {
-  
-  }, 1000)
-  
-  
-     }
-          else {
-  
-          }
-      });
-
-//------------------------ŞÜPHELİ-HESAP-----------------------\\     STG
-
-
-//-----------------------TAG-ROL----------------------\\     STG
-
-client.on("userUpdate", async (stg, yeni) => {
-  var sunucu = client.guilds.cache.get('SUNUCU ID'); // Buraya Sunucu ID
-  var uye = sunucu.members.cache.get(yeni.id);
-  var ekipTag = "TAG"; // Buraya Ekip Tag
-  var ekipRolü = "TAGLI ROL ID"; // Buraya Ekip Rolünün ID
-  var logKanali = "TAG ALINCA MESAJ GİDİCEK KANAL İD"; // Loglanacağı Kanalın ID
-
-  if (!sunucu.members.cache.has(yeni.id) || yeni.bot || stg.username === yeni.username) return;
-  
-  if ((yeni.username).includes(ekipTag) && !uye.roles.cache.has(ekipRolü)) {
-    try {
-      await uye.roles.add(ekipRolü);
-      await uye.send(`Tagımızı aldığın için teşekkürler! Aramıza hoş geldin.`);
-      await client.channels.cache.get(logKanali).send(new Discord.MessageEmbed().setColor('GREEN').setDescription(`${yeni} adlı üye tagımızı alarak aramıza katıldı!`));
-    } catch (err) { console.error(err) };
-  };
-  
-  if (!(yeni.username).includes(ekipTag) && uye.roles.cache.has(ekipRolü)) {
-    try {
-      await uye.roles.remove(uye.roles.cache.filter(rol => rol.position >= sunucu.roles.cache.get(ekipRolü).position));
-      await uye.send(`Tagımızı bıraktığın için ekip rolü ve yetkili rollerin alındı! Tagımızı tekrar alıp aramıza katılmak istersen;\nTagımız: **${ekipTag}**`);
-      await client.channels.cache.get(logKanali).send(new Discord.MessageEmbed().setColor('RED').setDescription(`${yeni} adlı üye tagımızı bırakarak aramızdan ayrıldı!`));
-    } catch(err) { console.error(err) };
-  };
-});
-
-//----------------------TAG-KONTROL----------------------\\     STG    
-
-client.on("guildMemberAdd", member => {
-  let sunucuid = "SUNUCU ID"; //Buraya sunucunuzun IDsini yazın
-  let tag = "TAG"; //Buraya tagınızı yazın
-  let rol = "TAGLI ROL ID"; //Buraya tag alındığı zaman verilecek rolün IDsini yazın
-  let channel = client.guilds.cache.get(sunucuid).channels.cache.find(x => x.name == 'auto-tag-role'); //tagrol-log yerine kendi log kanalınızın ismini yazabilirsiniz
-if(member.user.username.includes(tag)){
-member.roles.add(rol)
-  const tagalma = new Discord.MessageEmbed()
-      .setColor("GREEN")
-      .setDescription(`<@${member.id}> adlı kişi sunucumuza taglı şekilde katıldı, o doğuştan beri bizden !`)
-      .setTimestamp()
-     client.channels.cache.get('TAG ALINCA MESAJ GİDİCE KANAL İD').send(tagalma)
-}
-})
-
-//-----------------------TAG-KONTROL----------------------\\     STG    
